902d3aa5319f0b01a9aca006be3445f6243a1c5b
Esteve Julià <ejulimelis@ebay.com>
2016-05-26 23:23:02 +0200

    Refactor: remove unused class NormalFakeAlarm

--- src/test/java/NormalFakeAlarm.java
+++
@@ -1,9 +0,0 @@
-import tddmicroexercises.tirepressuremonitoringsystem.Alarm;
-
-public class NormalFakeAlarm extends Alarm {
-
-    @Override
-    protected double getPsiPressureValue() {
-        return 20;
-    }
-}
